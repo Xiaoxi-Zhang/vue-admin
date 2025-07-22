9c482aa7492777430617e4b370c413916ab763e9
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2025-07-22 18:25:31 +0800

    添加员工弹框实现+获取渲染角色列表

--- src/views/System/Employee/index.vue
+++ src/views/System/Employee/index.vue
@@ -7,7 +7,7 @@
       <el-button type="primary">查询</el-button>
     </div>
     <div class="create-container">
-      <el-button type="primary">添加员工</el-button>
+      <el-button type="primary" @click="addEmployee">添加员工</el-button>
     </div>
     <!-- 表格区域 -->
     <div class="table">
@@ -41,17 +41,79 @@
         @current-change="pageChange"
       />
     </div>
-    <!-- 添加员工 -->
+    <div>
+      <!-- 添加员工 -->
+      <el-dialog
+        title="添加员工"
+        width="480px"
+        :visible="dialogVisible"
+        :close-on-click-modal="false"
+        @close="closeDialog"
+        @open="openDialog"
+      >
+        <!-- 表单接口 -->
+        <div class="form-container">
+          <el-form ref="addForm" :model="addForm" :rules="addFormRules" label-width="80px">
+            <el-form-item label="员工姓名" prop="name">
+              <el-input v-model="addForm.name" />
+            </el-form-item>
+            <el-form-item label="登录账号" prop="userName">
+              <el-input v-model="addForm.userName" />
+            </el-form-item>
+            <el-form-item label="联系方式" prop="phonenumber">
+              <el-input v-model="addForm.phonenumber" />
+            </el-form-item>
+            <el-form-item label="分配角色" prop="roleId">
+              <el-select v-model="addForm.roleId" placeholder="请选择角色">
+                <el-option
+                  v-for="item in roleList"
+                  :key="item.roleId"
+                  :label="item.roleName"
+                  :value="item.roleId"
+                />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="员工状态" prop="status">
+              <el-radio-group v-model="addForm.status">
+                <el-radio :label="0">禁用</el-radio>
+                <el-radio :label="1">启用</el-radio>
+              </el-radio-group>
+            </el-form-item>
+          </el-form>
+        </div>
+        <template #footer>
+          <el-button size="mini" @click="dialogVisible = false">取 消</el-button>
+          <el-button size="mini" type="primary">确 定</el-button>
+        </template>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
 <script>
 import { getEmployeeListAPI } from '@/api/employee'
+import { getRoleListAPI } from '@/api/system'
 
 export default {
   name: 'Employee',
   data() {
     return {
+      dialogVisible: false, // 控制弹框显示
+      roleList: [], // 角色列表
+      addForm: {
+        name: '',
+        userName: '',
+        phonenumber: '',
+        status: 1,
+        roleId: null
+      },
+      addFormRules: {
+        name: [{ required: true, message: '请输入员工姓名', trigger: 'blur' }],
+        userName: [{ required: true, message: '请输入登录账号', trigger: 'blur' }],
+        phonenumber: [{ required: true, message: '请输入联系方式', trigger: 'blur' }],
+        roleId: [{ required: true, message: '请分配角色', trigger: 'change' }],
+        status: [{ required: true, message: '请选择员工状态', trigger: 'change' }]
+      },
       employeeList: [],
       params: {
         name: '',
@@ -65,6 +127,17 @@ export default {
     this.getEmployeeList()
   },
   methods: {
+    async openDialog() {
+      const res = await getRoleListAPI()
+      // console.log(res)
+      this.roleList = res.data
+    },
+    closeDialog() {
+      this.dialogVisible = false
+    },
+    addEmployee() {
+      this.dialogVisible = true
+    },
     indexMethod(index) {
       return (this.params.page - 1) * this.params.pageSize + index + 1
     },
